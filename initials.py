748409a0fdc7d459185652d7c46cc00ad9a03d26
Shortgoose108 <2425402556@live.gctu.edu.gh>
2026-05-30 17:14:55 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
 
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+# 
+# Name: Padmon Adomako Antwi
+# STUDENT ID: 2425402556
+# Email: paddyadomako246@gmail.com
+# Grader: Augustus
+#
+# On my honor, Augustus Buckman,this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 def main():
-  pass #code goes here
-
-
+    pass #code goes here
+    
+    print()
+    print("...PAA")
+    print()
+    print("...PPPPPPPPPP.." + "..." + ".." + "..." +  "....AAAA...." + "..." + ".." + "..." + "....AAAA...." + "..." + "..")
+    print("...PP........PP" + "..." + ".." + "..." +  "...AA..AA..." + "..." + ".." + "..." + "...AA..AA..." + "..." + "..") 
+    print("...PP........PP" + "..." + ".." + "..." +  "..AA....AA.." + "..." + ".." + "..." + "..AA....AA.." + "..." + "..")
+    print("...PPPPPPPPPP.." + "..." + ".." + "..." +  ".AA......AA." + "..." + ".." + "..." + ".AA......AA." + "..." + "..")
+    print("...PP.........." + "..." + ".." + "..." +  "AA........AA" + "..." + ".." + "..." + "AA........AA" + "..." + "..")
+    print("...PP.........." + "..." + ".." + "..." +  "AAAAAAAAAAAA" + "..." + ".." + "..." + "AAAAAAAAAAAA" + "..." + "..")
+    print("...PP.........." + "..." + ".." + "..." +  "AA........AA" + "..." + ".." + "..." + "AA........AA" + "..." + "..")
+    print("...PP.........." + "..." + ".." + "..." +  "AA........AA" + "..." + ".." + "..." + "AA........AA" + "..." + "..")
+    print("...PP.........." + "..." + "**" + "..." +  "AA........AA" + "..." + "**" + "..." + "AA........AA" + "..." + "**")
+    print("...PP.........." + "..." + "**" + "..." +  "AA........AA" + "..." + "**" + "..." + "AA........AA" + "..." + "**")
+    print()
 
 main()
+
+
